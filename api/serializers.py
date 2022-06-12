59d8c3fde6dd66c1421922b6866be4d0272aee4f
cleo <batista.cleo@gmail.com>
2022-06-12 19:57:28 -0300

    fixed read_only_fields

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from rest_framework import serializers
 from .models import Provider, ServiceArea
 
@@ -7,10 +6,11 @@ class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provider
         fields = "__all__"
-        read_only_fields = ("id")
+        read_only_fields = ("id",)
+
 
 class ServiceAreaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceArea
         fields = "__all__"
-        read_only_fields = ("id")
+        read_only_fields = ("id",)
