23184463be6c7e7cacfbfaf8fb241cf0177c5b0d
cleo <batista.cleo@gmail.com>
2022-06-12 19:56:10 -0300

    defined str name for models

--- api/models.py
+++ api/models.py
@@ -1,15 +1,25 @@
 from django.db import models
 from django.contrib.gis.db import models as gmodels
 
+
 class Provider(models.Model):
     name = models.CharField(max_length=128)
     email = models.EmailField()
     phone = models.CharField(max_length=128)
-    language = models.CharField(max_length=128)    
+    language = models.CharField(max_length=128)
     currency = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class ServiceArea(models.Model):
     name = models.CharField(max_length=128)
-    provider = models.ForeignKey(Provider, related_name='provider', on_delete=models.CASCADE)
+    provider = models.ForeignKey(
+        Provider, related_name="provider", on_delete=models.CASCADE
+    )
     price = models.FloatField()
     area = gmodels.PolygonField()
+
+    def __str__(self) -> str:
+        return f"{self.provider.name} - {self.name}"
