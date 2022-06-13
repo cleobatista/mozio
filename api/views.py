9f88ad652687a1d54cf83a9c29f3244d3c7ca1d1
cleo <batista.cleo@gmail.com>
2022-06-12 21:58:25 -0300

    more simple code in create and update methods

--- api/views.py
+++ api/views.py
@@ -25,13 +25,7 @@ class ServiceAreaViewSet(viewsets.ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED, headers=headers
-            )
+            return super().create(request, *args, **kwargs)
         except ValueError as error:
             return Response(
                 data={"detail": error.args[0]}, status=status.HTTP_400_BAD_REQUEST
@@ -39,20 +33,7 @@ class ServiceAreaViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         try:
-            partial = kwargs.pop("partial", False)
-            instance = self.get_object()
-            serializer = self.get_serializer(
-                instance, data=request.data, partial=partial
-            )
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-
-            if getattr(instance, "_prefetched_objects_cache", None):
-                # If 'prefetch_related' has been applied to a queryset, we need to
-                # forcibly invalidate the prefetch cache on the instance.
-                instance._prefetched_objects_cache = {}
-
-            return Response(serializer.data)
+            return super().update(request, *args, **kwargs)
         except ValueError as error:
             return Response(
                 data={"detail": error.args[0]}, status=status.HTTP_400_BAD_REQUEST
