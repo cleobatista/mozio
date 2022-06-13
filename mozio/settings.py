939bf7f01e0b707620de81cdff756e63cf26f758
cleo <batista.cleo@gmail.com>
2022-06-12 23:51:48 -0300

    fixed error in whitenoise settings

--- mozio/settings.py
+++ mozio/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 STATIC_URL = "static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
